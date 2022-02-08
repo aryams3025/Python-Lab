ff6a530ed26a47730cdf6fde3bb5048520a51d35
aryams3025 <80573229+aryams3025@users.noreply.github.com>
2022-02-08 11:58:48 +0530

    Update p3.py

--- CO4/p3.py
+++ CO4/p3.py
@@ -1,17 +1,23 @@
-class Rectangle :
-    def __init__(self,length,breadth):
-        self.__length = length
-        self.__breadth = breadth
-        self.__area = length*breadth
-
-    def __lt__(self, obj):
-
-        if (self.__area<obj.__area):
-            return "Rectangle 1 has larger area"
+class rectangle:
+    def __init__(self,l,b):
+        self.l=l
+        self.b=b
+    def area(self):
+        self.a=self.l*self.b
+    def __lt__(self, other):
+        if(self.a < other.a):
+            return "ob1 is lessthan ob2"
         else:
-            return "Rectangle 1 does not have larger area"
+            return "ob2 is less than ob1"
+
+i=int(input("enter the length of rectangle 1:"))
+m=int(input("enter the breadth of rectangle 1:"))
+x=int(input("enter the length of rectangle 2:"))
+y=int(input("enter the breadth of rectangle 2:"))
 
 
-rec1 = Rectangle(7,4)
-rec2 = Rectangle(123,21)
-print(rec1>rec2)              s
+r1=rectangle(i,m)
+r1.area()
+r2=rectangle(x,y)
+r2.area()        
+print(r1<r2)
