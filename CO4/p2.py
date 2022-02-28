ba235deead44d7607f755efdb1c28594b57d00af
aryams3025 <80573229+aryams3025@users.noreply.github.com>
2022-02-28 19:23:55 +0530

    Update p2.py

--- CO4/p2.py
+++ CO4/p2.py
@@ -1,44 +1,26 @@
-class customer:
-    def getdata(self,name,accno,acctype,balance):
+class Bankaccount:
+    def __init__(self,name):
         self.name=name
-        self.accno=accno
-        self.acctype=acctype
-        self.balance=balance
-    def displayCustomer(self):
-        print("Customer Name:", self.name)
-        print("Account Number:", self.accno)
-        print("Account Type:", self.acctype)
-        print("Balance amount:", self.balance)
-    def deposit(self,amount):
-        self.balance=self.balance+amount
-    def withdrawal(self,amount):
-        if self.balance-amount<0:
-            print("Insuffiecent fund")
-        else:
-            self.balance=self.balance-amount
-ch=0
-while ch!=5:
-    print("1.New customer")
-    print("2.Deposit")
-    print("3.Withdrawal")
-    print("4.Display")
-    print("5.Exit")
-    ch=int(input("Enter your choice:"))
-    if ch==1:
-        obj=customer()
-        n=input("Enter Name:")
-        no=int(input("Enter the account Number:"))
-        t=input("Enter the account type(SB/C):")
-        b=int(input("Enter the amount:"))
-        obj.getdata(n,no,t,b)
-    if ch==2:
-        b=int(input("Enter the amount to be deposited:"))
-        obj.deposit(b)
-    if ch==3:
-        b=int(input("Enter the amount to be withdrawn:"))
-        obj.withdrawal(b)
-    if ch==4:
-        obj.displayCustomer()
-    else:
-        print("Program Terminated")
-
+        self.balance=0
+        print(self.name,"account created successfully")
+    def deposit(self):
+        self.amount=int(input("Enter the amount to be deposited: "))
+        self.balance+=self.amount
+        print("Available balance: ",self.balance)
+    def withdraw(self):
+        self.withdraw=int(input("Enter the amount to be withdrawn: "))
+#        if self.balance<self.withdraw:
+#           print("Insufficent balance")
+#        else:
+#            self.balance-=self.withdraw
+#            print(self.withdraw)
+#            print("Current balance: ",self.balance)
+        self.balance-=self.withdraw
+        print(self.withdraw)
+        print("Current balance: ",self.balance)
+    def display(self):
+        print(self.balance)
+user=Bankaccount("Arya")
+user.display()
+user.deposit()
+user.withdraw()
