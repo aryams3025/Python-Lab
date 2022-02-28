ce0000e60e28756819444bbb272e23833c6db657
aryams3025 <80573229+aryams3025@users.noreply.github.com>
2022-02-28 19:27:49 +0530

    Update p4.py

--- CO4/p4.py
+++ CO4/p4.py
@@ -1,27 +1,17 @@
 class Time:
     def __init__(self,hour,minute,second):
-        self.__hour=hour
-        self.__minute=minute
-        self.__second=second
+        self.hour=hour
+        self.minute=minute
+        self.second=second
+    def __add__(self,other):
+        return (self.hour+other.hour,self.minute+other.minute,self.second+other.second)
 
-    def __add__(self,obj):
-        self.__hour += obj.__hour
-        self.__minute += obj.__minute
-        self.__second += obj.__second
-
-        if(self.__second>=60):
-            extra_minute =  int(self.__second/60)
-            self.__second = self.__second%60
-            self.__minute += extra_minute
-
-        if (self.__minute >= 60):
-            extra_hour = int(self.__minute / 60)
-            self.__minute = self.__minute % 60
-            self.__hour += extra_hour
-
-        return "Hour(s) : "+str(self.__hour)+" | Minute(s) : "+str(self.__minute)+" |  Second(s) : "+str(self.__second)
-
-obj1 = Time(6,3,4)
-obj2  = Time(4,89,11)
-
-print(obj1+obj2)
+hour=int(input("Enter hour:"))
+minute=int(input("Enter minute:"))
+second=int(input("Enter second:"))
+time1=Time(hour,minute,second)
+hour=int(input("Enter hour:"))
+minute=int(input("Enter minute:"))
+second=int(input("Enter second:"))
+time2=Time(hour,minute,second)
+print("Total time:",time1+time2)
