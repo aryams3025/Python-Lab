7595d23f076e81c1c69b6cb52bbf910c31b62107
aryams3025 <80573229+aryams3025@users.noreply.github.com>
2022-02-28 19:19:37 +0530

    Update P1.py

--- CO4/P1.py
+++ CO4/P1.py
@@ -1,32 +1,24 @@
 class rectangle:
-    def _init_(self,length,breadth):
+    def __init__(self,length,breadth):
         self.length=length
         self.breadth=breadth
-        
     def area(self):
-        self.a=self.length*self.breadth
-        print("Area is :",self.a)
-        return self.a
-    def perimeter(self):
-        self.p=2*(self.length+self.breadth)
-        print("Perimeter is:",self.p)
-i=int(input("enter the length of rectangle 1:"))
-m=int(input("enter the breadth of rectangle 1:"))
-x=int(input("enter the length of rectangle 2:"))
-y=int(input("enter the breadth of rectangle 2:"))
-
-
-r1=rectangle(i,m)
-e=r1.area()
-r1.perimeter()
-
-r2=rectangle(x,y)
-x=r2.area()
-r2.perimeter()
-        
-if e>x:
-    print("area of rectangle 1 is great")
-elif e<x:
-    print("area of rectangle 2 is great")
-else :
-     print("both area are same")
+        return self.length*self.breadth
+    def peri(self):
+        return 2*(self.length+self.breadth)
+l1=int(input("Enter the Length of first Rectangle "))
+b1=int(input("Enter the Breadth of first Rectangle "))
+l2=int(input("Enter Length of second Rectangle "))
+b2=int(input("Enter Breadth of second Rectangle "))
+r1=rectangle(l1,b1)
+r2=rectangle(l2,b2)
+print("Area of Rectangle one ",r1.area())
+print("Area of Rectangle two ",r2.area())
+print("Perimeter of Rectangle one ",r1.peri())
+print("Perimeter of Rectangle two ",r2.peri())
+if(r1.area()<r2.area()):
+    print("Area of Rectangle one is Smaller than the Second one")
+elif(r1.area()==r2.area()):
+    print("Area are Equal")
+else:
+    print("Area of Rectangle one is Larger than the Second one")
